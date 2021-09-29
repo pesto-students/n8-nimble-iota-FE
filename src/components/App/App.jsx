ff1db2f25f4cef5292330658fdeeea4eda115e15
Jyotirmaya Sahu <jyotirmayasahu38@gmail.com>
2021-09-29 22:00:12 +0530

    Added Button Input DDl

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,11 +3,27 @@ import { Provider } from "react-redux";
 import store from "../../redux/store";
 import IndexRouting from "../../route/IndexRouting";
 import "./App.less";
-import '../themes/themes.less'
+import "../themes/themes.less";
+import AppInput from "../Common/AppInput/AppInput";
+import AppDropDown from "../Common/AppDropDown/AppDropDown";
 function App() {
     return (
         <>
             <Provider store={store}>
+                <div
+                    style={{
+                        margin: 10,
+                    }}
+                >
+                    <AppDropDown
+                        options={[
+                            { label: "x", value: "x" },
+                            { label: "y", value: "y" },
+                        ]}
+                        // defaultValue="x"
+                        placeholder="Select"
+                    />
+                </div>
                 <IndexRouting />
             </Provider>
         </>
---
+++ src/components/Common/AppButton/AppButton.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button } from "antd";
+import PropTypes from "prop-types";
+import styles from "./Button.module.less";
+
+const AppButton = ({ type, size, loading, onClick, children }) => {
+    return (
+        <React.Fragment>
+            <Button
+                type={type ? type : "primary"}
+                size={size}
+                loading={loading}
+                className={styles.btn}
+                onClick={onClick}
+            >
+                {children}
+            </Button>
+        </React.Fragment>
+    );
+};
+
+export default AppButton;
+
+AppButton.propTypes = {
+    type: PropTypes.oneOf("primary", "dashed", "text", "link", "default"),
+    size: PropTypes.oneOf("large", "middle", "small"),
+    loading: PropTypes.bool,
+    children: PropTypes.elementType,
+    onClick: PropTypes.func,
+};
