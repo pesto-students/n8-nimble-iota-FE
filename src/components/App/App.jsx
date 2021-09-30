8e23a83489f01b8c5fab876e9eded104255cfd2e
Jyotirmaya Sahu <jyotirmayasahu38@gmail.com>
2021-09-30 15:19:22 +0530

    Added navbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,27 +4,12 @@ import store from "../../redux/store";
 import IndexRouting from "../../route/IndexRouting";
 import "./App.less";
 import "../themes/themes.less";
-import AppInput from "../Common/AppInput/AppInput";
-import AppDropDown from "../Common/AppDropDown/AppDropDown";
+import NavBar from "../Common/NavBar/NavBar";
 function App() {
     return (
         <>
             <Provider store={store}>
-                <div
-                    style={{
-                        margin: 10,
-                    }}
-                >
-                    <AppDropDown
-                        options={[
-                            { label: "x", value: "x" },
-                            { label: "y", value: "y" },
-                        ]}
-                        // defaultValue="x"
-                        placeholder="Select"
-                    />
-                </div>
-                <IndexRouting />
+                <NavBar />
             </Provider>
         </>
     );
--- src/route/IndexRouting.js
+++ src/route/IndexRouting.js
@@ -9,8 +9,9 @@ import { loginUserSuccess } from "../redux";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 import AccountActivate from "../components/Auth/AccountActivate";
+import PropTypes from "prop-types";
 
-function IndexRouting() {
+function IndexRouting({ children }) {
     let dispatch = useDispatch();
     useEffect(() => {
         const loggedInUser = localStorage.getItem("user");
@@ -18,16 +19,21 @@ function IndexRouting() {
             const foundUser = JSON.parse(loggedInUser);
             dispatch(loginUserSuccess(foundUser));
             axios.defaults.headers.common["Authorization"] =
-        "Bearer " + foundUser.token;
+                "Bearer " + foundUser.token;
         }
     }, [dispatch]);
     return (
         <>
             <BrowserRouter>
+                {children}
                 <Switch>
                     <Route exact path="/signin" component={SignIn} />
                     <Route exact path="/signup" component={SignUp} />
-                    <Route exact path="/forgotpassword" component={ForgotPassword} />
+                    <Route
+                        exact
+                        path="/forgotpassword"
+                        component={ForgotPassword}
+                    />
                     <Route
                         exact
                         path="/auth/activate/:token"
@@ -48,5 +54,9 @@ function IndexRouting() {
         </>
     );
 }
+
+IndexRouting.propTypes = {
+    children: PropTypes.elementType,
+};
 
 export default IndexRouting;
