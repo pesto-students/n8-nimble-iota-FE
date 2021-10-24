47d35b96d500d9aed99f36643c15fd39be2b3e7f
Vishnu Thiyagarajan <vishnu.thg@gmail.com>
2021-10-24 12:51:45 +0530

    baclogs remove header tabs

--- src/components/Page/Project/Project.jsx
+++ src/components/Page/Project/Project.jsx
@@ -2,7 +2,7 @@ import { Tabs } from "antd";
 import React from "react";
 import { useSelector } from "react-redux";
 import { Route, Switch } from "react-router";
-import { Redirect, useParams } from "react-router-dom";
+import { Redirect, useLocation, useParams } from "react-router-dom";
 import Backlogs from "src/components/Page/Backlog/Backlogs";
 import Meeting from "src/components/Page/Meeting/Meeting";
 import "src/components/Page/Project/Project.less";
@@ -12,45 +12,42 @@ import { useRouting } from "src/util/hooks";
 const Project = () => {
     const { projectId } = useParams();
     const { TabPane } = Tabs;
-    const initialRoute = "/scrum_board";
     const { navigate, path, url } = useRouting();
+    let initialRoute = url + "/scrum_board";
+    const location = useLocation();
+    const loc = location.pathname.split("/").pop();
+    if (loc !== projectId) initialRoute = url + "/" + loc;
     const projects = useSelector((state) => state.projectList.projects);
     const currentProject = projects.find((e) => e._id === projectId);
     const currentSprint = useSelector((state) => state.project.sprint.selectedSprint);
     const { status } = currentSprint || {};
-
+    const openTab = (key) => {
+        navigate(key, true, true);
+    };
     return (
         <>
             <Switch>
-                <Route exact path={path} render={() => <Redirect to={`${url}${initialRoute}`} />} />
+                <Route exact path={path} render={() => <Redirect to={initialRoute} />} />
                 <Route path={`${path}/meet`}>
                     <Meeting />
                 </Route>
                 <Route path={`${path}/backlogs`} component={Backlogs} />
-
-                <Route path={path}>
-                    <Tabs
-                        defaultActiveKey={`${url}${initialRoute}`}
-                        onChange={(key) => {
-                            navigate(key, true, true);
-                        }}
-                    >
-                        {ScrumRoutes.map((route, index) => (
-                            <React.Fragment key={index}>
-                                {route.allowedStatus.includes(status) && (
-                                    <TabPane key={`${url}${route.path}`} tab={route.name}>
-                                        <Route component={route.component} path={`${path}${route.path}`} />
-                                    </TabPane>
-                                )}
-                            </React.Fragment>
-                        ))}
-                    </Tabs>
-                </Route>
             </Switch>
+            {path.endsWith(":projectId") && (
+                <Tabs activeKey={initialRoute} onChange={openTab}>
+                    {ScrumRoutes.map((route, index) => (
+                        <React.Fragment key={index}>
+                            {route.allowedStatus.includes(status) && (
+                                <TabPane key={`${url}${route.path}`} tab={route.name}>
+                                    <route.component />
+                                </TabPane>
+                            )}
+                        </React.Fragment>
+                    ))}
+                </Tabs>
+            )}
         </>
     );
 };
-
-Project.propTypes = {};
 
 export default Project;
